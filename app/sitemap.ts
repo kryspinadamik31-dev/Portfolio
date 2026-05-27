c0947fea5a62266d4a7942c2978ba7a5c3675028
Kryspin <kryspinadamik31@gmail.com>
2026-05-27 20:11:19 +0200

    fix seo and sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.nextbyte.com.pl",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.nextbyte.com.pl/projekty",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.nextbyte.com.pl/kontakt",
+      lastModified: new Date(),
+    },
+  ];
+}
